e0be658e05008e0279e9b01e857d9278bc7ba1a1
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-25 12:35:41 +0530

    togglebar added

---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Menu } from "lucide-react";
+
+import Sidebar from "@/components/Sidebar";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+<Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
