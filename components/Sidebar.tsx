9ff18d8d45bbf75892659f123c64c8eeb8e44615
panditdhamdhere <dhamdherepandit@gmail.com>
2023-08-01 09:15:54 +0530

    counter, progressbar etc added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -15,6 +15,7 @@ import {
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
+import { FreeCounter } from "@/components/FreeCounter";
 
 const montserrate = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -63,7 +64,11 @@ const routes = [
   },
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
   const pathname = usePathname();
 
   return (
@@ -84,8 +89,9 @@ const Sidebar = () => {
               key={route.href}
               className={cn(
                 "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
-                pathname === route.href ? "text-white bg-white/10"
-                : "text-zinc-400"
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
               )}
             >
               <div className="flex items-center flex-1">
@@ -96,6 +102,7 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   );
 };
