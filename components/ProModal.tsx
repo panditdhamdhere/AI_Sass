cbc3b984d9c466d1020594332bea2a15bd628ba3
panditdhamdhere <dhamdherepandit@gmail.com>
2023-08-04 11:31:19 +0530

    toast added to button and subscription

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -25,6 +25,7 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 const tools = [
   {
@@ -70,7 +71,7 @@ export const ProModal = () => {
 
       window.location.href = (await response).data.url;
     } catch (error) {
-      console.log(error, "STRIPE_CLIENT_ERROR");
+      toast.error("Something went wrong");
     } finally {
       setLoading(false);
     }
