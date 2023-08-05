0214c90c75f7b889bcc6e2c21792b7a76094d25d
panditdhamdhere <dhamdherepandit@gmail.com>
2023-08-05 09:29:22 +0530

    hero component finished

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,12 @@
+import { LandingHero } from "@/components/LandingHero";
 import { LandingNavbar } from "@/components/LandingNavbar";
 
 const LandingPage = () => {
   return (
     <div className="h-full">
       <LandingNavbar />
+      <LandingHero />
+      
     </div>
   );
 };
